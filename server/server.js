8371387a535bbfda1a1634e11c51fa1cde862664
dinesh <dineshmanideep@gmail.com>
2025-10-31 01:00:04 +0530

    adding_domain_parameter to fix cookie

--- server/server.js
+++ server/server.js
@@ -83,6 +83,7 @@ app.use(
       secure: process.env.NODE_ENV === "production", // Always true in production
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // "none" required for cross-origin
       // Don't set domain for cross-origin cookies - browser handles it automatically
+      domain: process.env.NODE_ENV === "production" ? '.vercel.app' : ".localhost",
     },
   }),
 );
